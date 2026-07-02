d64274a467ca94596cb8db8e1a62cdeb30198022
Suronjit Sutradhar <suronjit02@gmail.com>
2026-07-02 23:31:30 +0600

    binary search : day 9

--- day-9/binarySearch.js
+++ day-9/binarySearch.js
@@ -3,3 +3,23 @@ Description: Write a function binarySearch(arr, target) that searches a sorted a
 Example:
 Input: [1,3,5,7,9], target=7  → Output: 3
 Hint: Use left and right pointers; check the middle element each iteration. */
+
+const binarySearch = (arr, target) => {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+};
+
+console.log(binarySearch([1, 3, 5, 7, 9], 7));
