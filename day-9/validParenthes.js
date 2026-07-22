e006f806bb411036f5478b6ad44b82bc9b529961
Suronjit Sutradhar <suronjit02@gmail.com>
2026-07-22 21:50:17 +0600

    validParenthes check: day-9 problem

--- day-9/validParenthes.js
+++ day-9/validParenthes.js
@@ -1,6 +1,33 @@
 /* Problem 44: Valid Parentheses  [Medium]
 Description: Write a function isValidParentheses(str) that returns true if the string has valid, balanced parentheses, brackets, and braces.
 Example:
-Input: '()[]{}'  → Output: trueInput: '([)]'    → Output: false
+Input: '()[]{}'  → Output: true
+Input: '([)]'    → Output: false
 Hint: Use a stack (array). Push opening brackets, pop and compare for closing ones.
  */
+
+
+function isValidParentheses(str) {
+  const stack = [];
+  const pairs = {
+    ")": "(",
+    "]": "[",
+    "}": "{",
+  };
+
+  for (const char of str) {
+    if (char === "(" || char === "[" || char === "{") {
+      stack.push(char);
+    } else if (char === ")" || char === "]" || char === "}") {
+      if (stack.pop() !== pairs[char]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+}
+
+
+console.log(isValidParentheses("()[]{}")); // true  
+console.log(isValidParentheses("([)]")); // false   
