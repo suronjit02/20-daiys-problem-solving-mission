fe0176490680b7c3b134aa0fce520a69f78f32ac
Suronjit Sutradhar <suronjit02@gmail.com>
2026-07-24 00:47:59 +0600

    problem groupArray : day8

--- day-8/groupArray.js
+++ day-8/groupArray.js
@@ -3,3 +3,26 @@ Description: Write a function groupBy(arr, key) that groups an array of objects 
 Example:
 groupBy([{type:'fruit',name:'apple'},{type:'veg',name:'carrot'},{type:'fruit',name:'mango'}], 'type')// {fruit: [...], veg: [...]}
 Hint: Use reduce() and build an object where each key maps to an array. */
+
+const groupBy = (arr, key) => {
+  return arr.reduce((result, obj) => {
+    const groupKey = obj[key];
+    if (!result[groupKey]) {
+      result[groupKey] = [];
+    }
+    result[groupKey].push(obj);
+    return result;
+  }, {});
+};
+
+console.log(
+  groupBy(
+    [
+      { type: "fruit", name: "apple" },
+      { type: "veg", name: "carrot" },
+      { type: "fruit", name: "mango" },
+    ],
+    "type",
+  ),
+);
+// Output: { fruit: [{ type: 'fruit', name: 'apple' }, { type: 'fruit', name: 'mango' }], veg: [{ type: 'veg', name: 'carrot' }] }
