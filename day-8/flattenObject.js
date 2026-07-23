ab8b7f674a6bc306e164203e1f4241882d0adbe3
Suronjit Sutradhar <suronjit02@gmail.com>
2026-07-23 10:34:30 +0600

    day-8 : flattenObject

--- day-8/flattenObject.js
+++ day-8/flattenObject.js
@@ -3,3 +3,24 @@ Description: Write a function flattenObject(obj) that takes a deeply nested obje
 Example:
 Input: {a: {b: {c: 1}}}Output: {'a.b.c': 1}
 Hint: Use recursion; build the key by joining parent keys with dots. */
+
+const flattenObject = (obj, parentKey = "", result = {}) => {
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      const newKey = parentKey ? `${parentKey}.${key}` : key;
+      if (
+        typeof obj[key] === "object" &&
+        obj[key] !== null &&
+        !Array.isArray(obj[key])
+      ) {
+        flattenObject(obj[key], newKey, result);
+      } else {
+        result[newKey] = obj[key];
+      }
+    }
+  }
+  return result;
+};
+
+console.log(flattenObject({ a: { b: { c: 1 } } })); // { 'a.b.c': 1 }
+console.log(flattenObject({ a: 1, b: { c: 2, d: { e: 3 } } })); // { 'a': 1, 'b.c': 2, 'b.d.e': 3 }
