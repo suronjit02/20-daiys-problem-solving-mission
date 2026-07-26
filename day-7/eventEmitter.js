f43e3b495b0b6bb0e3d3562b536d7ccf04b02867
Suronjit Sutradhar <suronjit02@gmail.com>
2026-07-26 20:09:04 +0600

    eventEmitter : day-7

--- day-7/eventEmitter.js
+++ day-7/eventEmitter.js
@@ -5,3 +5,27 @@ const emitter = new EventEmitter();emitter.on('greet', name => console.log('Hell
 Hint: Store listeners in an object where keys are event names and values are arrays of functions.
  */
 
+
+const EventEmitter = function() {
+  this.events = {};
+}
+
+EventEmitter.prototype.on = function(event, listener) {
+  if (!this.events[event]) {
+    this.events[event] = [];
+  }
+  this.events[event].push(listener);
+};
+
+EventEmitter.prototype.emit = function(event, ...args) {
+  if (this.events[event]) {
+    this.events[event].forEach(listener => listener(...args));
+  }
+};
+
+EventEmitter.prototype.off = function(event, listener) {
+  if (this.events[event]) {
+    this.events[event] = this.events[event].filter(l => l !== listener);
+  }
+};
+
